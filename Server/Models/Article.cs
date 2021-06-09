a4b86ff20a719ea98fb6d05992abd6d28476d90c
Easwaramoorthy <spemoorthy@gmail.com>
2021-06-09 06:13:45 +0530

    Initial Setup

---
+++ Server/Models/Article.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPEMoorthy.Server.Models
+{
+    public class Article
+    {
+        [Key]
+        long Id { get; set; }
+        string Category { get; set; }
+        string Title { get; set; }
+        string Description { get; set; }
+        string URI { get; set; }
+        bool IsActive { get; set; }
+    }
+}
