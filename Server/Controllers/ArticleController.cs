9070fa7f9d6cf1257c6b11f88251e882bfa39ad3
Easwaramoorthy <spemoorthy@gmail.com>
2021-08-11 08:34:14 +0530

    Github API Introduced and could able to read Private repo files.

--- Server/Controllers/ArticleController.cs
+++ Server/Controllers/ArticleController.cs
@@ -1,9 +1,11 @@
 ﻿using Markdig;
 using Microsoft.AspNetCore.Mvc;
+using Octokit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -33,12 +35,44 @@ namespace SPEMoorthy.Server.Controllers
         public async Task<string> Get(int id)
         {
             // Pull markdown file from the specified url
-            HttpClient client = _clientFactory.CreateClient();
-            string strmd = await client.GetStringAsync("https://raw.githubusercontent.com/hey-red/Markdown/master/README.md");
+            //HttpClient client = _clientFactory.CreateClient();
+            //string strmd = await client.GetStringAsync("https://raw.githubusercontent.com/hey-red/Markdown/master/README.md");
+
+            string strMarkdown = "Welcome";
+            Dictionary<int, String> myArticles = new Dictionary<int, string>();
+            myArticles.Add(1, "001_SPEMoorthy.md");
+            myArticles.Add(2, "002_Startup_Onboarding_Attrition.md");
+            myArticles.Add(3, "003_Fix_SQL_Database_CRC_Error.md");
+            myArticles.Add(4, "004_Mini_Project_GST_Invoicing.md");
+
+
+            var gitHubClient = new GitHubClient(new ProductHeaderValue("SPEMoorthy"));
+            var tokenAuth = new Credentials("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");
+            gitHubClient.Credentials = tokenAuth;
+
+            string articleName = "";
+
+            if (myArticles.TryGetValue(id, out articleName))
+            {
+                try
+                {
+                    byte[] articleContent = await gitHubClient.Repository.Content.GetRawContent("SPEMoorthyDotCom", "ArticleContent", articleName);
+                    strMarkdown = Encoding.Default.GetString(articleContent);
+                }
+                catch (Exception e)
+                {
+                    strMarkdown = e.Message;
+                }
+            }
+            else
+            {
+                strMarkdown = "The requested article with id " + id + "is not found!";
+            }
+
 
             // Configure the pipeline with all advanced extensions active
             var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
-            var result = Markdown.ToHtml(strmd, pipeline);
+            var result = Markdown.ToHtml(strMarkdown, pipeline);
 
             return result;
         }
